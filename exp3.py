3ecb520c870272e53f649b0f78d4ec99f9538df4
SimonRosen42 <30725995+SimonRosen42@users.noreply.github.com>
2022-04-25 10:18:46 +0200

    Experiment run & multiprocessing added to exp3

--- exp3.py
+++ exp3.py
@@ -9,115 +9,53 @@ import multiprocessing
 
 import itertools
 
+BASE_PKL_PATH = "Q_pkls/corridors/4/"
+MAXITERS = 50000
 
-# def train_task(task_id):
-#     base_pkl_path = "Q_pkls/corridors/2/"
-#
-#     env_kwargs = {
-#         "n_agents": 2,
-#         "n_actions": 5,
-#         "goal_reward": 2,
-#         "collide_reward": -0.1,
-#         "joint_start_state": [(1, 1), (11, 11)],  # It currently doesn't work if this isn't specified
-#         "random_starts": True,
-#         "rooms_type": "corridors"
-#     }
-#
-#     maxiters = 1000
-#     hyper_params = {
-#         "maxiter": maxiters,
-#
-#         "epsilon": 0.25,  # Will be ignored if is_eps_decay = True
-#
-#         "is_printing": True,
-#
-#         "is_eps_decay": True,
-#         "eps_start": 1.0,
-#         "eps_end": 0.01,
-#         "eps_decay_rate": 1.0 / maxiters
-#     }
-#
-#     all_test_starts = [MA4Rooms.TL_CNR, MA4Rooms.TR_CNR, MA4Rooms.BL_CNR, MA4Rooms.BR_CNR]
-#     all_test_joint_starts = list(itertools.product(all_test_starts, all_test_starts))
-#     all_test_joint_starts = [list(el) for el in all_test_joint_starts]
-#     # Remove joint_starts with collisions
-#     all_test_joint_starts = list(filter(lambda x: x[0] != x[1], all_test_joint_starts))
-#
-#     g_all = [MA4Rooms.CORRIDOR_BLC, MA4Rooms.CORRIDOR_BRC,
-#              MA4Rooms.CORRIDOR_TLC, MA4Rooms.CORRIDOR_TRC]
-#
-#     if task_id == "A":
-#         # -------- #
-#         #  Task A  #
-#         # -------- #
-#         # Agent 1 must go to a bottom goal (i.e., BLC or BRC)
-#         # Agent 2 can go to any goal
-#         g_bottom = [MA4Rooms.CORRIDOR_BLC, MA4Rooms.CORRIDOR_BRC]
-#
-#         joint_goals = list(itertools.product(g_bottom, g_all))
-#         joint_goals = [list(el) for el in joint_goals]
-#
-#         env_kwargs["joint_goals"] = joint_goals
-#         env_A = MA4Rooms(**env_kwargs)
-#
-#         env_A = MA4RoomsWrapper(env_A)
-#
-#         Q_A, stats_A = Goal_Oriented_Q_learning(env_A, print_prefix=task_id, **hyper_params)
-#
-#         library.save_extended_Q(Q_A, base_pkl_path + "Q_A.pkl")
-#
-#         benchmark.follow_policies(env_A, Q_A, all_test_joint_starts, "trajs/Q_A_traj.txt")
-#
-#         return Q_A, stats_A
-#     else:
-#         # -------- #
-#         #  Task B  #
-#         # -------- #
-#         # Agent 1 can go to any goal
-#         # Agent 2 must go to a right goal (i.e., TRC or BRC)
-#         g_right = [MA4Rooms.CORRIDOR_TRC, MA4Rooms.CORRIDOR_BRC]
-#         joint_goals = list(itertools.product(g_all, g_right))
-#         joint_goals = [list(el) for el in joint_goals]
-#
-#         env_kwargs["joint_goals"] = joint_goals
-#         env_B = MA4Rooms(**env_kwargs)
-#
-#         env_B = MA4RoomsWrapper(env_B)
-#
-#         Q_B, stats_B = Goal_Oriented_Q_learning(env_B, print_prefix=task_id, **hyper_params)
-#         library.save_extended_Q(Q_B, base_pkl_path + "Q_B.pkl")
-#
-#         benchmark.follow_policies(env_B, Q_B, all_test_joint_starts, "trajs/Q_B_traj.txt")
-#
-#         return Q_B, stats_B
+ENV_KWARGS = {
+    "n_agents": 2,
+    "n_actions": 5,
+    "goal_reward": 2,
+    "collide_reward": -0.1,
+    "joint_start_state": [(1, 1), (11, 11)],  # It currently doesn't work if this isn't specified
+    "random_starts": True,
+    "rooms_type": "corridors"
+}
+
+HYPER_PARAMS = {
+    "maxiter": MAXITERS,
+
+    "epsilon": 0.25,  # Will be ignored if is_eps_decay = True
+
+    "is_printing": True,
+
+    "is_eps_decay": True,
+    "eps_start": 1.0,
+    "eps_end": 0.01,
+    "eps_decay_rate": 1.0 / MAXITERS
+}
 
 
 def main():
-    base_pkl_path = "Q_pkls/corridors/4/"
+    base_pkl_path = BASE_PKL_PATH
+    # base_pkl_path = "Q_pkls/corridors/4/"
 
-    env_kwargs = {
-        "n_agents": 2,
-        "n_actions": 5,
-        "goal_reward": 2,
-        "collide_reward": -0.1,
-        "joint_start_state": [(1, 1), (11, 11)],  # It currently doesn't work if this isn't specified
-        "random_starts": True,
-        "rooms_type": "corridors"
-    }
+    env_kwargs = ENV_KWARGS
+    maxiters = MAXITERS
 
-    maxiters = 50000
-    hyper_params = {
-        "maxiter": maxiters,
+    # env_kwargs = {
+    #     "n_agents": 2,
+    #     "n_actions": 5,
+    #     "goal_reward": 2,
+    #     "collide_reward": -0.1,
+    #     "joint_start_state": [(1, 1), (11, 11)],  # It currently doesn't work if this isn't specified
+    #     "random_starts": True,
+    #     "rooms_type": "corridors"
+    # }
 
-        "epsilon": 0.25,  # Will be ignored if is_eps_decay = True
+    # maxiters = 50000
 
-        "is_printing": True,
 
-        "is_eps_decay": True,
-        "eps_start": 1.0,
-        "eps_end": 0.01,
-        "eps_decay_rate": 1.0 / maxiters
-    }
 
     all_test_starts = [MA4Rooms.TL_CNR, MA4Rooms.TR_CNR, MA4Rooms.BL_CNR, MA4Rooms.BR_CNR]
     all_test_joint_starts = list(itertools.product(all_test_starts, all_test_starts))
@@ -189,7 +127,7 @@ def test_composition(Q_A, Q_B, env):
     n_actions = env.action_space.n
 
     AND_PTL = partial(AND, n_actions=n_actions)
-    NOT_PTL = partial(NOT, n_actions=n_actions, rmin=-100, rmax=env.rmax)
+    NOT_PTL = partial(NOT, n_actions=n_actions, rmin=-500, rmax=env.rmax)
 
     print("# A AND B #")
     Q_comp_and = AND_PTL(Q_A, Q_B)
@@ -203,15 +141,106 @@ def test_composition(Q_A, Q_B, env):
     pass
 
 
-def test_fn(x):
-    return x * 2
+def train_task(task_id):
+    maxiters = MAXITERS
+    env_kwargs = ENV_KWARGS
+    hyper_params = HYPER_PARAMS
+
+    # hyper_params = {
+    #     "maxiter": maxiters,
+    #
+    #     "epsilon": 0.25,  # Will be ignored if is_eps_decay = True
+    #
+    #     "is_printing": True,
+    #
+    #     "is_eps_decay": True,
+    #     "eps_start": 1.0,
+    #     "eps_end": 0.01,
+    #     "eps_decay_rate": 1.0 / maxiters
+    # }
+
+    all_test_starts = [MA4Rooms.TL_CNR, MA4Rooms.TR_CNR, MA4Rooms.BL_CNR, MA4Rooms.BR_CNR]
+    all_test_joint_starts = list(itertools.product(all_test_starts, all_test_starts))
+    all_test_joint_starts = [list(el) for el in all_test_joint_starts]
+    # Remove joint_starts with collisions
+    all_test_joint_starts = list(filter(lambda x: x[0] != x[1], all_test_joint_starts))
+
+    g_all = [MA4Rooms.CORRIDOR_BLC, MA4Rooms.CORRIDOR_BRC,
+             MA4Rooms.CORRIDOR_TLC, MA4Rooms.CORRIDOR_TRC]
+
+    if task_id == "A":
+        # -------- #
+        #  Task A  #
+        # -------- #
+        # Agent 1 must go to a bottom goal (i.e., BLC or BRC)
+        # Agent 2 can go to any goal
+        g_bottom = [MA4Rooms.CORRIDOR_BLC, MA4Rooms.CORRIDOR_BRC]
+
+        joint_goals = list(itertools.product(g_bottom, g_all))
+        joint_goals = [list(el) for el in joint_goals]
+
+        env_kwargs["joint_goals"] = joint_goals
+        env_A = MA4Rooms(**env_kwargs)
+
+        env_A = MA4RoomsWrapper(env_A)
+
+        Q_A, stats_A = Goal_Oriented_Q_learning(env_A, print_prefix=task_id, **hyper_params)
+
+        library.save_extended_Q(Q_A, BASE_PKL_PATH + "Q_A.pkl")
+
+        benchmark.follow_policies(env_A, Q_A, all_test_joint_starts, "trajs/Q_A_traj.txt")
+
+        # return Q_A, stats_A
+    else:
+        # -------- #
+        #  Task B  #
+        # -------- #
+        # Agent 1 can go to any goal
+        # Agent 2 must go to a right goal (i.e., TRC or BRC)
+        g_right = [MA4Rooms.CORRIDOR_TRC, MA4Rooms.CORRIDOR_BRC]
+        joint_goals = list(itertools.product(g_all, g_right))
+        joint_goals = [list(el) for el in joint_goals]
+
+        env_kwargs["joint_goals"] = joint_goals
+        env_B = MA4Rooms(**env_kwargs)
+
+        env_B = MA4RoomsWrapper(env_B)
+
+        Q_B, stats_B = Goal_Oriented_Q_learning(env_B, print_prefix=task_id, **hyper_params)
+        library.save_extended_Q(Q_B, BASE_PKL_PATH + "Q_B.pkl")
+
+        benchmark.follow_policies(env_B, Q_B, all_test_joint_starts, "trajs/Q_B_traj.txt")
+
+        # return Q_B, stats_B
 
 
-def test_multiproc():
-    with multiprocessing.Pool(processes=16) as pool:
-        results = pool.map(test_fn, range(16))
+def train_multiproc():
+    with multiprocessing.Pool(processes=2) as pool:
+        pool.map(train_task, ["A", "B"])
 
-    print(results)
+    Q_A, Q_B, env = load_Q(BASE_PKL_PATH)
+    test_composition(Q_A, Q_B, env)
+
+
+def load_Q(base_path):
+    env_kwargs = ENV_KWARGS
+    g_all = [MA4Rooms.CORRIDOR_BLC, MA4Rooms.CORRIDOR_BRC,
+             MA4Rooms.CORRIDOR_TLC, MA4Rooms.CORRIDOR_TRC]
+
+    g_bottom = [MA4Rooms.CORRIDOR_BLC, MA4Rooms.CORRIDOR_BRC]
+
+    joint_goals = list(itertools.product(g_bottom, g_all))
+    joint_goals = [list(el) for el in joint_goals]
+
+    env_kwargs["joint_goals"] = joint_goals
+    env = MA4Rooms(**env_kwargs)
+
+    env = MA4RoomsWrapper(env)
+
+    Q_A = library.load_extended_Q(base_path + "Q_A.pkl", env.action_space.n)
+    Q_B = library.load_extended_Q(base_path + "Q_B.pkl", env.action_space.n)
+
+    return Q_A, Q_B, env
 
 
 def test_load():
@@ -246,4 +275,5 @@ def test_load():
 
 if __name__ == '__main__':
     # main()
-    test_load()
+    # test_load()
+    train_multiproc()
